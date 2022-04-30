543eb7f8b28265d6378955b07e3e681aceeb6f44
thechidubem <kingsleychidubem042@gmail.com>
2022-04-30 17:42:12 +0100

    connected to the server

--- components/Auth/Input.js
+++ components/Auth/Input.js
@@ -17,8 +17,6 @@ function Input({
       </Text>
       <TextInput
         style={[styles.input, isInvalid && styles.inputInvalid]}
-        autoCapitalize={false}
-        // autoCapitalize="none"
         keyboardType={keyboardType}
         secureTextEntry={secure}
         onChangeText={onUpdateValue}
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import axios from 'axios';
+import {useEffect, useState, useContext} from "react";
+import { AuthContext } from '../store/Auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] =useState('');
+
+  const authCtx = useContext(AuthContext); 
+  const token = authCtx.token;
+
+useEffect(() => {
+  axios.get(
+    "https://react-native-project-d0246-default-rtdb.firebaseio.com/message.json?auth=" + token
+    ).then((response) => {
+     setFetchedMessage(response.data);
+    });
+}, [token])
+
   return (
     <View style={styles.rootContainer}>
+      <Image source={require("../assets/download.jpeg")} style={styles.image}/>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -22,5 +40,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 8,
+  },
+  image: {
+   borderRadius: 10,
   },
 });
